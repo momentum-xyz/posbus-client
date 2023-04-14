10499b369ae21b7447bc111c8b3f248bf84be315
Jorrit <js@odyssey.org>
2023-04-14 13:28:10 +0200

    feat(cli): optional guest user

--- cmd/standalone/main.go
+++ cmd/standalone/main.go
@@ -15,6 +15,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/momentum-xyz/posbus-client/pbc"
 	"github.com/momentum-xyz/ubercontroller/pkg/cmath"
 	"github.com/momentum-xyz/ubercontroller/pkg/posbus"
@@ -28,6 +29,7 @@ var URL = "http://localhost:4000/posbus"
 func main() {
 	backendArg := flag.String("backend", "http://localhost:4000", "The URL to the controller backend")
 	worldArg := flag.String("world", "975cb9ca-4dfa-4d35-adc2-198ed1f12555", "UUID of a world")
+	token := flag.String("token", "", "An authentication token")
 	flag.Parse()
 	backend, err := url.Parse(*backendArg)
 	if err != nil {
@@ -41,21 +43,17 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	resp, err := http.Post(backend.JoinPath("/api/v4/auth/guest-token").String(), "", nil)
-	if err != nil {
-		log.Fatalf("Error of getting guest token %+v\n", err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error %+v\n", err)
-	}
-
-	var u dto.User
-	err = json.Unmarshal(body, &u)
-	if err != nil {
-		log.Fatalf("Error %+v\n", err)
+	var user *dto.User
+	if *token != "" {
+		user, err = userFromToken(*token)
+		if err != nil {
+			log.Fatalf("Invalid userId %s", err)
+		}
+	} else {
+		user, err = guestUser(backend)
+		if err != nil {
+			log.Fatalf("guest user: %s", err)
+		}
 	}
 
 	//fmt.Printf("%+v\n", u)
@@ -64,10 +62,10 @@ func main() {
 	defer cancelConnection()
 	client := pbc.NewClient()
 	client.SetCallback(onMessage)
-	log.Printf("Connecting to %s as %s\n", URL, u.Name)
-	client.Connect(connectionCtx, URL, *u.JWTToken, umid.MustParse(u.ID))
+	log.Printf("Connecting to %s as %s\n", URL, user.Name)
+	client.Connect(connectionCtx, URL, *user.JWTToken, umid.MustParse(user.ID))
 
-	log.Printf("Teleporting %v to %s\n", u.Name, world)
+	log.Printf("Teleporting %v to %s\n", user.Name, world)
 	client.Send(
 		posbus.BinMessage(
 			&posbus.TeleportRequest{Target: world},
@@ -124,4 +122,36 @@ func onSignal(sig *posbus.Signal) error {
 		log.Printf("Unhandled signal %d\n", sig.Value)
 	}
 	return nil
+}
+
+func userFromToken(tokenString string) (*dto.User, error) {
+	fmt.Println(tokenString)
+	token, _ := jwt.ParseWithClaims(tokenString, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(""), nil
+	})
+	claims := token.Claims.(*jwt.StandardClaims)
+	return &dto.User{ // TODO: get with API call?
+		ID:       claims.Subject,
+		JWTToken: &tokenString,
+		Name:     claims.Subject,
+	}, nil
+}
+
+func guestUser(backend *url.URL) (*dto.User, error) {
+	resp, err := http.Post(backend.JoinPath("/api/v4/auth/guest-token").String(), "", nil)
+	if err != nil {
+		return nil, fmt.Errorf("guest token: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("guest token: %w", err)
+	}
+	var u dto.User
+	err = json.Unmarshal(body, &u)
+	if err != nil {
+		return nil, fmt.Errorf("guest token: %w", err)
+	}
+	return &u, nil
 }
