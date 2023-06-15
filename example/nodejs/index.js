747665e2809cd13c1720c0712ffaee9a31ae31d9
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2023-06-15 12:12:20 +0300

    refactor(example-nodejs): clearer structure, sending message

--- example/nodejs/index.js
+++ example/nodejs/index.js
@@ -1,6 +1,6 @@
 import "websocket-polyfill";
 // import { PBClient } from "@momentum-xyz/posbus-client";
-import { PBClient } from "../../dist/index.mjs";
+import { PBClient, MsgType } from "../../dist/index.mjs";
 import fs from "fs";
 
 const [userId, token] = process.argv.slice(2);
@@ -16,28 +16,34 @@ const buffer = fs.readFileSync(
 );
 
 async function main(userId, token) {
-  try {
-    const client = new PBClient((event) => {
-      console.log(`PosBus message [${userId}]:`, event.data);
-    });
+  const client = new PBClient((event) => {
+    console.log(`PosBus message [${userId}]:`, event.data);
+  });
 
-    await client.load(buffer);
+  await client.load(buffer);
 
-    console.log(`PosBus client loaded [${userId}]`, client);
-    await client.connect(`https://dev.odyssey.ninja/posbus`, token, userId);
-    // await client.connect(`http://localhost:4000/posbus`, token, userId);
-    // await client.connect(`ws://localhost:4000/posbus`, token, userId);
+  console.log(`PosBus client loaded [${userId}]`, client);
+  await client.connect(`https://dev.odyssey.ninja/posbus`, token, userId);
+  // await client.connect(`http://localhost:4000/posbus`, token, userId);
+  // await client.connect(`ws://localhost:4000/posbus`, token, userId);
 
-    await sleep(500);
-    console.log(`PosBus client teleport [${userId}]`);
-    await client.teleport("00000000-0000-8000-8000-000000000005");
-  } catch (err) {
-    console.error(err);
-  }
+  await sleep(500);
+  console.log(`PosBus client teleport [${userId}]`);
+  await client.teleport("00000000-0000-8000-8000-000000000005");
+
+  await sleep(3000);
+
+  console.log(`PosBus client send MY_TRANSFORM [${userId}]`);
+  await client.send(MsgType.MY_TRANSFORM, {
+    position: { x: 0, y: 0, z: 5 },
+    rotation: { x: 0, y: 0, z: 0 },
+  });
 }
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-main(userId, token);
+main(userId, token).catch((err) => {
+  console.error(err);
+});
